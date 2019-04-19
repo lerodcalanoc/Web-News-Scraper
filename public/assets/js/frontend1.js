cfa954ffb1eadb87c34aa217265bf5debe7905a3
lerodcalanoc <lerodcalanoc@gmail.com>
2019-04-19 03:48:10 -0700

    added files

---
+++ public/assets/js/frontend1.js
@@ -0,0 +1,271 @@
+const homeUrl = "/";
+const defaultTimeOut = 2000;
+// This Function Take the information From the Article in the DOM
+const getDOMArticle = (e) => {
+  const articleContainer = e.target.parentElement.parentElement;
+  const articleLink = articleContainer.children[0];
+  const id = articleContainer.id;
+  const title = articleLink.innerText;
+  const link = articleLink.href;
+
+  return {
+    id,
+    title,
+    link
+  };
+};
+
+const drawAndShowModal = (title, message) => {
+  // Draw the response Message in the Modal
+  $("#messageModal .modal-title").text(title);
+  $("#messageModal .modal-body > p").text(message);
+  // Show the Modal
+  $("#messageModal").modal("show");
+};
+
+const drawAndShowNotesModal = (title, message, article) => {
+  // Draw the response Message in the Modal
+  $("#notesModal .modal-title").text(article.title);
+  $("#notesModal .modal-body > p").text(article.brief);
+  // Draw the article Notes
+  article.notes.forEach( note => {
+    // console.log(note.body);
+    const noteDiv = $("<div>")
+      .addClass("note-div d-flex justify-content-between my-2")
+      .attr("data-articleid", article._id)
+      .attr("id",`div-note-${note._id}`);
+    const delNoteBtn = $("<button>")
+      .addClass("btn btn-sm btn-danger delete-note")
+      .html("&times;")
+      .attr("data-articleid", article._id)
+      .attr("id",`btn-del-note-${note._id}`);
+    noteDiv.text(note.body);
+    noteDiv.append(delNoteBtn);
+    $("#notesModal .modal-body .notes").append(noteDiv);
+  });
+
+  // Add Submit Button
+  const submitNoteBtn = $("<button>")
+    .addClass("btn btn-primary btn-add-note")
+    .attr("data-articleid", article._id)
+    .text("Submit");
+  $("#notesModal .modal-footer").append(submitNoteBtn);
+
+  // Show the Modal
+  $("#notesModal").modal("show");
+};
+
+// Clear modal dismiss 
+$('#messageModal').on('hidden.bs.modal', function (e) {
+  $("#messageModal .modal-body > p").text("");
+  $("#messageModal .modal-title").text("");
+  location.reload();
+});
+
+// Clear modal dismiss 
+$('#notesModal').on('hidden.bs.modal', function (e) {
+  $("#notesModal .modal-body > p").text("");
+  $("#notesModal .modal-body .notes").empty();
+  $("#notesModal .modal-title").text("");
+  $("#notesModal .modal-footer").empty();
+  $("#noteInput").val("");
+  location.reload();
+});
+
+// Scrap Button Handler
+const scrapButtonHandler = (e) => {
+  e.preventDefault();
+  // Execute Scraping Routine via Ajax
+  $.ajax(
+    {
+      url: "/api/scrape",
+      method: "GET"
+    }
+  )
+    .then(response => {
+      const message = response.msg ? response.msg : response.error;
+      const title = response.msg ? "" : "An Error Occurred in the API endpoint";
+      const myTimeOut = response.msg ? 2000 : 5000;
+      drawAndShowModal(title, message);
+      setTimeout(() => window.location.replace(homeUrl), myTimeOut);
+    })
+    .catch(error => {
+      // console.log(error);
+      drawAndShowModal("An Error Happen", "Status Code: " + JSON.stringify(error.status));
+    });  
+};
+
+// Event Delegation
+// https://davidwalsh.name/event-delegate
+// $(document).on("click", "#scrapingBtn", scrapButtonHandler); //jQuery
+document.getElementById("mainNavbar").addEventListener("click", e => {
+  if(e.target && e.target.id==="scrapingBtn"){
+    // console.log(e.target);
+    scrapButtonHandler(e);
+  }
+});
+
+// displayArticle Handler
+const displayArticle = (e) => {
+  e.preventDefault();
+};
+
+// saveArticle Handler
+const saveArticle = (e) => {
+  e.preventDefault();
+
+  const {id, title, link} = getDOMArticle(e);
+  // console.log(getDOMArticle(e));
+  
+  // Ajax call to the API - Save Article to Saved Articles Collection
+  $.ajax(
+    {
+      url: `/api/save_article/${id}`,
+      method: "POST",
+      data: {id:id}
+    }
+  )
+    .then(response => {
+      // console.log(response);
+      drawAndShowModal("Article Saved", title);
+      setTimeout(() => location.reload(), defaultTimeOut);
+    })
+    .catch(error => {
+      // console.log(error);
+      drawAndShowModal("An Error Happen", "Status Code: " + JSON.stringify(error.status));
+    });  
+
+};
+
+// deleteArticle Handler
+const deleteArticleFromSaved = (e) => {
+  e.preventDefault();
+  const {id, title, link} = getDOMArticle(e);
+  // console.log(getDOMArticle(e));
+  
+  // Ajax call to the API - Delete Article
+  $.ajax(
+    {
+      url: `/api/delete_article/${id}`,
+      method: "POST",
+      data: {id:id}
+    }
+  )
+    .then(response => {
+      // console.log(response);
+      drawAndShowModal("Article Saved", title);
+      setTimeout(() => location.reload(), defaultTimeOut);
+    })
+    .catch(error => {
+      // console.log(error);
+      drawAndShowModal("An Error Happen", "Status Code: " + JSON.stringify(error.status));
+    });  
+
+};
+
+// viewNotes Handler
+const viewNotes = (e) => {
+  e.preventDefault();
+  // Get the Information of the Article
+  const {id, title, link} = getDOMArticle(e);
+  // console.log(`/api/article/${id}`);
+  // Get the Notes From the Article
+  $.ajax(
+    {
+      url: `/api/article/${id}`,
+      method: "GET"
+    }
+  )
+    .then(response => {
+      drawAndShowNotesModal(id, title, response);
+    })
+    .catch(error => {
+      // console.log(error);
+      drawAndShowModal("An Error Happen", "Status Code: " + JSON.stringify(error.status));
+    });  
+};
+
+document.getElementById("articleList").addEventListener("click", e => {
+  if(e.target && e.target.classList.contains("article-div")){
+    // console.log(e.target);
+    // console.log(e.target.id);
+    // scrapButtonHandler(e);
+  }
+  else if(e.target && e.target.classList.contains("save-article")){
+    saveArticle(e);
+  }
+  else if(e.target && e.target.classList.contains("delete-article")){
+    deleteArticleFromSaved(e);
+  }
+  else if(e.target && e.target.classList.contains("view-notes")){
+    viewNotes(e);
+  }
+});
+
+// addNote Handler
+const addNote = (e) => {
+  e.preventDefault();
+  const articleId = e.target.dataset.articleid;
+  const noteInput = document.getElementById("noteInput");
+  const title = "";
+  const body = noteInput.value
+
+  $.ajax({
+    url: `/article-add-note/${articleId}`,
+    method: "POST",
+    data: {
+      title,
+      body
+    }
+  })
+    .then(response => {
+      // console.log("Response:", response)
+      const lastNote = response.notes.reverse()[0];
+      const noteDiv = $("<div>")
+        .addClass("note-div d-flex justify-content-between my-2")
+        .attr("data-articleid", articleId)
+        .attr("id",`div-note-${lastNote}`);
+      const delNoteBtn = $("<button>")
+        .addClass("btn btn-sm btn-danger delete-note")
+        .html("&times;")
+        .attr("data-articleid", articleId)
+        .attr("id",`btn-del-note-${lastNote}`);
+      noteDiv.text(body);
+      noteDiv.append(delNoteBtn);
+      $("#notesModal .modal-body .notes").append(noteDiv);
+      $("#noteInput").val("");
+    })
+    .catch(error => console.log("Error:", error));
+};
+
+// deleteNote Handler
+const deleteNote = (e) => {
+  e.preventDefault();
+  // IDs are generated in drawAndShowNotesModal() in this File.
+  const btnId = e.target.id;
+  const articleId = e.target.dataset.articleid;
+  const noteId = btnId.replace("btn-del-note-", "");
+  const noteDiv = document.getElementById(`div-note-${noteId}`);
+  // Remove the Note From the Modal
+  noteDiv.remove();
+  // Remove the Note From the Database by Calling the API.
+  $.ajax({
+    url: "/article-remove-note",
+    method: "POST",
+    data: {
+      articleId,
+      noteId
+    }
+  })
+    .then(result => /*console.log(result)*/ result)
+    .catch(err => console.log(err));
+};
+
+document.getElementById("notesModal").addEventListener("click", e => {
+  if(e.target && e.target.classList.contains("delete-note")){
+    deleteNote(e);
+  }
+  else if(e.target && e.target.classList.contains("btn-add-note")){
+    addNote(e);
+  }
+});
---
+++ routes/apiRoutes.js
@@ -0,0 +1,184 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+module.exports = (app, db) => {
+  app.get("/api/scrape", (req, res) => {
+    // First, we grab the body of the html with axios
+    axios.get("https://sdtimes.com/category/latest-news/")
+      .then(response => {
+        // Then, we load that into cheerio and save it to $ for a shorthand selector
+        const $ = cheerio.load(response.data);
+  
+        $(".latestnewslist").each(function() {
+          // Save an empty result object
+          const result = {};
+    
+          // Add the text and href of every link, and save them as properties of the result object
+          result.image = $(this).children(".thm").children("a").children("img").attr("src");
+          result.title = $(this).children(".btmborder").children("h4").children("a").text();
+          result.link = $(this).children(".btmborder").children("h4").children("a").attr("href");
+          result.brief = $(this).children(".btmborder").children("p").text().replace("… continue reading", "...");
+  
+          // Custom Implementation of Find or Create
+          // This will prevent to duplicate Articles
+          db.Article.findOne({title:result.title})
+            .then(article => {
+              // console.log(article);
+              if(!article){
+                // console.log(result.title, "Not Found");
+                // Create a new Article using the `result` object built from scraping
+                db.Article.create(result)
+                  .then(dbArticle => {
+                    // View the added result in the console
+                    // eslint-disable-next-line no-console
+                    // console.log(dbArticle);
+                    dbArticle;
+                  })
+                  .catch(function(err) {
+                    // If an error occurred, log it
+                    // eslint-disable-next-line no-console
+                    console.log(err);
+                  });
+              }
+            })
+            .catch(error => res.json({"error": error}));
+        });
+    
+        // Send a message to the client
+        // res.send("Scrape Complete");
+        res.json({"msg": "Scrape Completed"});
+      })
+      .catch(axiosError => {
+        res.json({"error": axiosError.message});
+      });
+  });
+  
+  // Route for getting all Scraped Articles from the db
+  app.get("/articles", function(req, res) {
+    // Grab every document in the Articles collection
+    db.Article.find({saved:false})
+      .then(function(dbArticle) {
+        // If we were able to successfully find Articles, send them back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
+  
+  
+  // Route to Get All Articles
+  app.get("/api/articles/:option", function(req, res) {
+    const urlOption = req.params.option;
+    const queryOptions = ["all", "scraped", "saved"];
+    const removeOptions = ["remove-scraped", "remove-all"];
+    let query;
+
+    switch(urlOption){
+    case "all":
+      query = {};
+      break;
+    case "scraped":
+      query = {saved:false};
+      break;
+    case "saved":
+      query = {saved:true};
+      break;
+    case "remove-scraped":
+      query = {saved:false};;
+      break;
+    case "remove-all":
+      query = {};
+      break;
+    }
+
+    if (queryOptions.includes(urlOption)) {
+      db.Article.find(query)
+        .populate("notes")
+        .then(dbArticle => res.json(dbArticle))
+        .catch(err =>res.json(err));
+    } 
+    else if (removeOptions.includes(urlOption)) {
+      db.Article.remove(query)
+        .then(result => res.json(result))
+        .catch(err =>res.json(err));
+    } else {
+      res.json({msg: "GET /api/articles/:option NO OPTION MATCHING"});
+    }
+  });
+
+  // Route for saving Scraped Article to Saved Articles
+  app.post("/api/save_article/:id", function(req, res) {
+    db.Article.findByIdAndUpdate(req.params.id, {saved:true}, { new: true })
+      .then(dbArticle => res.json(dbArticle))
+      .catch(err => res.json(err));
+  });
+
+  // Route to Delete Article
+  app.post("/api/delete_article/:id", function(req, res) {
+    db.Article.findByIdAndRemove(req.params.id)
+      .then(dbArticle => res.json(dbArticle))
+      .catch(err => res.json(err));
+
+  app.post('/delete_all', (req,res) =>  {
+    Article.deleteMany({}, response => res.redirect('/'))
+  })
+  });
+
+  // Route for grabbing a specific Article by id, populate it with it's note
+  app.get("/api/article/:id", function(req, res) {
+    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+    db.Article.findOne({ _id: req.params.id })
+      // ..and populate all of the notes associated with it
+      .populate("notes")
+      .then(function(dbArticle) {
+        // If we were able to successfully find an Article with the given id, send it back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
+
+  // Add Note
+  // Route for saving/updating an Article's associated Note
+  app.post("/article-add-note/:id", function(req, res) {
+    // Create a new note and pass the req.body to the entry
+    db.Note.create(req.body)
+      .then(function(dbNote) {
+        // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+        // return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+        return db.Article.findOneAndUpdate({ _id: req.params.id }, { $push: { notes: dbNote._id } }, { new: true });
+      })
+      .then(function(dbArticle) {
+        // If we were able to successfully update an Article, send it back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
+
+  // Remove Note
+  // https://mongoosejs.com/docs/api.html#mongoosearray_MongooseArray-pull
+  // Route for saving/updating an Article's associated Note
+  app.post("/article-remove-note/", function(req, res) {
+    // Create a new note and pass the req.body to the entry
+    db.Article.findOneAndUpdate({ _id: req.body.articleId }, { $pull: { notes: req.body.noteId } }, { new: true })
+      .then(dbArticle => {
+        db.Note.findByIdAndRemove(req.body.noteId)
+          .then(dbNote => {
+            dbNote;
+          })
+          .catch(err => res.json(err));
+        res.json(dbArticle);
+      })
+      .catch(err => res.json(err));
+  });
+
+};
